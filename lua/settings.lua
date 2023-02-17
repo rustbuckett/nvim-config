a013c27d5b3a7de371d8e261665ba66207c81cbf
Russell Stinnett <rsstinnett@pm.me>
2023-02-17 05:52:55 -0500

    added some nvim setting like tabs and stuff

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,6 +1,29 @@
 --[ neovim ]--
 
+--[ Some interface stuff ]--
+vim.cmd [[hi Normal ctermbg=NONE]]
+vim.cmd [[hi VertSplit cterm=NONE]]
+vim.cmd [[hi EndOfBuffer ctermfg=NONE ctermbg=NONE]]
+vim.opt.fillchars={eob=' '}
+vim.wo.number = true
+--vim.opt.splitbelow = true
+
+vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 0
+vim.opt.expandtab = true
+
+--[ Spell Checker Stuff ]--
+vim.opt.spelllang='en_us'
+vim.opt.spellfile='$HOME/.config/nvim/spell/en.utf-8.add'
+--vim.opt.thesaurus=$HOME/.vim/thesaurus/thesaurus.txt
+vim.opt.spellsuggest='best,9'
+vim.cmd [[hi clear SpellBad]]
+vim.cmd [[hi clear SpellCap]]
+vim.cmd [[hi SpellBad ctermfg=red]]
+vim.cmd [[hi SpellCap ctermfg=green]]
+
 --[ gruvbox ]--
--- I may move this to another file
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
