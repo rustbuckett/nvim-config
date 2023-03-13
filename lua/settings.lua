7603bcf56d2d790b12a08a0af109b3b293363e86
Russell Stinnett <rsstinnett@pm.me>
2023-03-13 17:39:25 -0400

    Added some interface settings from vim book

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,14 +1,25 @@
 --[ neovim ]--
 
+--[ gruvbox ]--
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+
 --[ Some interface stuff ]--
-vim.cmd [[hi Normal ctermbg=NONE]]
+vim.cmd [[hi Normal guibg=NONEctermbg=NONE]]
 vim.cmd [[hi VertSplit cterm=NONE]]
 vim.cmd [[hi EndOfBuffer ctermfg=NONE ctermbg=NONE]]
 vim.opt.fillchars={eob=' '}
 vim.wo.number = true
 vim.opt.termguicolors = true
 
-vim.opt.number = true
+vim.opt.number = true -- show numbers
+vim.opt.title = true -- set the window title to filename
+vim.opt.cursorline = true -- highlight current line
+vim.opt.smartcase = true -- ignore case when searching unless I type capital
+vim.opt.linebreak = true -- wrap lines at convenient points
+vim.opt.scrolloff = 5 -- number of lines to keep above and below
+
+--[ tab stuff
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 0
@@ -24,6 +35,3 @@ vim.cmd [[hi clear SpellCap]]
 vim.cmd [[hi SpellBad ctermfg=red]]
 vim.cmd [[hi SpellCap ctermfg=green]]
 
---[ gruvbox ]--
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
