00351f23a20f886b0ca7c826ff2a78775f7e2f7c
Russell Stinnett <rsstinnett@pm.me>
2023-03-22 13:18:58 -0500

    corrected plugin entry for gitsigns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -58,7 +58,7 @@ return require('packer').startup(function()
 	use {
 	    'lewis6991/gitsigns.nvim',
 	    config = function()
-	      require('gitsigns').setup()
+	      require('config.gitsigns')
 	    end
 	}
 
