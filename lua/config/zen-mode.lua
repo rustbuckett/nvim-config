6ea1c63347f6ab0db7d4a138821383591e77366f
Russell Stinnett <rsstinnett@pm.me>
2023-07-05 14:25:52 -0400

    some updates to zen-mode

--- lua/config/zen-mode.lua
+++ lua/config/zen-mode.lua
@@ -1,18 +1,19 @@
 require("zen-mode").setup
 {
   window = {
-    backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+    backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
     -- height and width can be:
     -- * an absolute number of cells when > 1
     -- * a percentage of the width / height of the editor when <= 1
     -- * a function that returns the width or the height
-    width = 120, -- width of the Zen window
+    width = 80, -- width of the Zen window
+    --width = 120, -- width of the Zen window
     height = 1, -- height of the Zen window
     -- by default, no options are changed for the Zen window
     -- uncomment any of the options below, or add other vim.wo options you want to apply
     options = {
       -- signcolumn = "no", -- disable signcolumn
-      -- number = false, -- disable number column
+      number = false, -- disable number column
       -- relativenumber = false, -- disable relative numbers
       -- cursorline = false, -- disable cursorline
       -- cursorcolumn = false, -- disable cursor column
@@ -27,6 +28,7 @@ require("zen-mode").setup
       enabled = true,
       ruler = false, -- disables the ruler text in the cmd line area
       showcmd = false, -- disables the command in the last line of the screen
+      textwidth = 80,
     },
     twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
     gitsigns = { enabled = false }, -- disables git signs
