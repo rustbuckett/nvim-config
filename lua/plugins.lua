c30a8da1b17993014019f6fbfe03bc54298e85c5
downtime <downtime@samurai.localnet>
2023-11-04 10:43:30 -0400

    I know I probably made a lot of changes, but the biggest one is that I migrated from Packer to Lazy.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,100 +1,100 @@
-return require('packer').startup(function()
-    -- Packer can manage itself
-	use { 'wbthomason/packer.nvim' }
+require("lazy").setup({
 
     -- A nice dashboard
-	use { 'goolord/alpha-nvim',
-          -- requires = { 'nvim-tree/nvim-web-devicons' },
-    }
+	{
+		'goolord/alpha-nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+    },
 
     -- Telescope - Fuzzy file search
     -- Currently no custom config
-	use {
-	    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	    requires = { {'nvim-lua/plenary.nvim'} }
-	}
+	{
+	    'nvim-telescope/telescope.nvim',
+		version = "0.1.1",
+	    dependencies = {'nvim-lua/plenary.nvim'}
+	},
 
     -- My favorite theme
-	use { "ellisonleao/gruvbox.nvim" }
+	'ellisonleao/gruvbox.nvim',
 
     -- Filesystem browsing in a sidebar
-	use {
+	{
 	    'nvim-tree/nvim-tree.lua',
-	    requires = { {'nvim-tree/nvim-web-devicons'} },
-	}
+	    dependencies = {'nvim-tree/nvim-web-devicons'},
+	},
 
     -- Better syntax highlighting among other things
-	use {
+	{
         'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
-	}
+        build = ':TSUpdate',
+	},
 
     -- A more informative and configurable status line
-	use {
+	{
 	    'nvim-lualine/lualine.nvim',
-	    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
-	}
+	    dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
+	},
 
     -- Insert those handy indentation alignment lines
-	use { 'lukas-reineke/indent-blankline.nvim' }
+	'lukas-reineke/indent-blankline.nvim',
 
     -- Real time file change indicators for git
-	use { 'lewis6991/gitsigns.nvim' }
+	{ 'lewis6991/gitsigns.nvim' },
 
-    -- A REPL that runs in its own buffer
-	use { 'Vigemus/iron.nvim' } -- I don't know if I need this afterall. I may just use terminal
+    -- A REPL that builds in its own buffer
+	{ 'Vigemus/iron.nvim' }, -- I don't know if I need this afterall. I may just use terminal
 
     -- Manage your git repo from inside Neovim with a nice interface
-	use {
-        'NeogitOrg/neogit', 
-	    requires = {'nvim-lua/plenary.nvim'},
-	}
+	{
+        'NeogitOrg/neogit',
+	    dependencies = {'nvim-lua/plenary.nvim'},
+	},
 
     -- Focus mode for editing
-    use { "folke/zen-mode.nvim" }
+    { "folke/zen-mode.nvim" },
 
     -- Creates match pairs for quotes and brackets
-	use { "windwp/nvim-autopairs" }
+	{ "windwp/nvim-autopairs" },
 
     -- Clickable tabs for buffers
-    use {
-        'akinsho/bufferline.nvim', 
-        tag = "v3.*", 
-        requires = 'nvim-tree/nvim-web-devicons',
-    }
-	
+    {
+        'akinsho/bufferline.nvim',
+        version = "v3.*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+    },
+
     --[ I didn't realize I wanted these. Found on neovimcraft.com ]--
     -- Quick comments
-	use { 'numToStr/Comment.nvim' }
+	{ 'numToStr/Comment.nvim' },
 
-    -- TODO comments, just use keyword followed by ':'
-    use {
+    -- TODO comments, just keyword followed by ':'
+    {
         "folke/todo-comments.nvim",
-        requires = "nvim-lua/plenary.nvim",
-    }
+        dependencies = "nvim-lua/plenary.nvim",
+    },
 
-    use {
+    {
         "ThePrimeagen/refactoring.nvim",
-        requires = {
+        dependencies = {
             {"nvim-lua/plenary.nvim"},
             {"nvim-treesitter/nvim-treesitter"}
         },
-    }
+    },
 
     -- Preview markdown in a browser
-    use {
+    {
         "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    }
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
 
     -- Handles LSP, Linters, etc.
-    use { "williamboman/mason.nvim" }
-    use { "williamboman/mason-lspconfig.nvim" }
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
     
     -- giving nvim-cmp it's own section
-    use {
+    {
         "hrsh7th/nvim-cmp",
-        requires = {
+        dependencies = {
             {'neovim/nvim-lspconfig'},
             {'hrsh7th/cmp-nvim-lsp'},
             {'hrsh7th/cmp-buffer'},
@@ -104,13 +104,13 @@ return require('packer').startup(function()
             {'hrsh7th/vim-vsnip'},
             {'rafamadriz/friendly-snippets'}
         },
-    }
+    },
 
     -- markdown syntax hiding and other stuff
-    use { 'ixru/nvim-markdown' }
+    { 'ixru/nvim-markdown' },
 
     -- -- makes ordered and unordered lists easier
-    -- use {
+    -- {
     --     "gaoDean/autolist.nvim",
     --     ft = {
     --       "markdown",
@@ -125,4 +125,4 @@ return require('packer').startup(function()
 	-- TODO: grammarous (this is a vim tool, not lua)
     -- TODO: magma for Jupyter
 
-end)
+})
