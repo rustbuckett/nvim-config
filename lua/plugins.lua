923b03e3b2afd83d278ce82b2584be1245fd298b
Russell Stinnett <rsstinnett@pm.me>
2024-04-21 18:56:35 -0400

    added nvim-surround, using defaults

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -162,6 +162,19 @@ require("lazy").setup({
             "hrsh7th/nvim-cmp",
         },
     },
+
+    -- The Neovim version of Surround
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    }
+
     -- -- Github Copilot [[didn't realize it was a subscription!]]
     -- {
     --     "github/copilot.vim",
