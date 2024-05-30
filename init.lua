b0e129ffb3e2d777ea02912817863bed4efe4d33
Russell Stinnett <rsstinnett@pm.me>
2024-05-30 11:43:20 -0400

    changed the order of Lazy

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
-require('keybinds')
-require('plugins')
-require('config')
-require('settings')
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -15,4 +10,9 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
+require('keybinds')
+require('plugins')
+require('config')
+require('settings')
 
