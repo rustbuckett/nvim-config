0550c57e8b3d26591a4afe868d57c8cd6c8b206b
downtime <downtime@tomahawk.localnet>
2024-12-22 16:49:41 -0500

    nothing i do changes the VertSplit line

--- lua/settings.lua
+++ lua/settings.lua
@@ -5,11 +5,12 @@ vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
 
 --[ Some interface stuff ]--
--- vim.api.nvim_set_hl(0, "Normal", { ctermbg = "NONE", guibg = "NONE" })
--- vim.api.nvim_set_hl(0, "VertSplit", { ctermfg = "NONE", ctermbg = "NONE", guibg = "NONE", guifg = "NONE" })
-vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
-vim.cmd [[hi VertSplit cterm=NONE]]
-vim.cmd [[hi EndOfBuffer ctermfg=NONE ctermbg=NONE]]
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "VertSplit", { fg = "NONE", bg = "NONE" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "NONE", bg = "NONE" })
+-- vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
+-- vim.cmd [[hi VertSplit cterm=NONE]]
+-- vim.cmd [[hi EndOfBuffer ctermfg=NONE ctermbg=NONE]]
 vim.opt.fillchars={eob=' '}
 vim.wo.number = true
 vim.opt.termguicolors = true
